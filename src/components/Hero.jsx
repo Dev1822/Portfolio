4fbdf1b30e5c214818aba65191909f8e24b2aeb1
Dev1822 <dev.d.patel.cg@gmail.com>
2026-03-30 22:54:39 +0530

    Updated Resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ export default function Hero() {
                   />
                 </a>
                 <a
-                  href="https://drive.google.com/file/d/1B75jEsjhA6VWl2QxcVXvJIdww4gIy8FZ/view?usp=sharing"
+                  href="https://drive.google.com/file/d/13SFh3KZ-BdCW1Rk2lR6WbE5ZtZbUTbN0/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="group px-8 py-3 rounded-xl backdrop-blur-lg bg-white/5 border border-white/10 text-white font-semibold flex items-center justify-center gap-2 transition-all duration-300 hover:scale-105 hover:bg-white/10"
